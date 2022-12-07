517ad7fbf70775fdaa8dfb88c50030ef9bf17fa5
Syahruzein <syahruzein@example.com>
2022-12-08 01:03:39 +0700

    versi revisi sidang PA2

--- layouts/home/NavBar.vue
+++ layouts/home/NavBar.vue
@@ -1,39 +1,36 @@
 <template>
   <nav
-    class="navbary fixed-top navbar-expand-lg pt-2 pb-2"
+    class="navbar fixed-top navbar-expand-lg pt-2 pb-2"
     data-aos="fade-down"
     data-aos-duration="1000"
     :class="{ onScroll: !view.topOfPage }"
         >
-            <div class="container d-flex ">
-                    <div class="col">
-                        <a class="navbar-brandy fs-4" href="#" :class="{ 'onScroll': !view.topOfPage}"                        >
-                            <span class="logo">
-                                <img src="~/assets/img/logo/apr.svg" 
-                                    class="rounded" alt="..."
-                                    >
-                            </span>
-                        </a>
-                    </div>                    
-                    <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
-                        <ul class="navbar-nav">
-                            <div class="col-md-4">         
-                            <li class="nav-item"><a class="nav-linky fs-5" :class="{ 'onScroll': !view.topOfPage}" href="#myCarousel">Beranda</a></li>                
-                            </div>
-                            <div class="col-md-6">
-                            <li class="nav-item"><a class="nav-linky fs-5" :class="{ 'onScroll': !view.topOfPage}" href="#body">Alur Pengajuan</a></li>
-                            </div>
-                            <div class="col-md-6">
-                            <li class="nav-item"><a class="nav-linky fs-5" :class="{ 'onScroll': !view.topOfPage}" href="#FooterView">Tentang Kami</a></li>
-                            </div>
-                        </ul>
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#" :class="{ 'onScroll': !view.topOfPage}"                        >
+                <span class="logo">
+                    <img src="~/assets/img/logo/apr.svg" 
+                        class="rounded" alt="..."
+                        >
+                </span>
+            </a>                 
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"><i class="fas fa-bars fas-color" :class="{ 'onScroll': !view.topOfPage}"></i></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <div class="col-md-4">         
+                    <li class="nav-item"><a class="nav-linky fs-5" :class="{ 'onScroll': !view.topOfPage}" href="#myCarousel">Beranda</a></li>                
                     </div>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"><i class="fas fa-bars fas-color" :class="{ 'onScroll': !view.topOfPage}"></i></span>
-                    </button>
-            </div>
-
-        </nav>
+                    <div class="col-md-6">
+                    <li class="nav-item"><a class="nav-linky fs-5" :class="{ 'onScroll': !view.topOfPage}" href="#body">Alur Pengajuan</a></li>
+                    </div>
+                    <div class="col-md-6">
+                    <li class="nav-item"><a class="nav-linky fs-5" :class="{ 'onScroll': !view.topOfPage}" href="#FooterView">Tentang Kami</a></li>
+                    </div>
+                </ul>
+            </div>    
+        </div>                    
+    </nav>
 </template>
 
 <script>
